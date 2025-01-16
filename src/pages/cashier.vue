86ce7f540811c9411b6c1eecc97cc2571d8fec30
Thet-Htar-Thu-Zar <thethtarthuzar648@gmail.com>
2025-01-16 16:04:46 +0630

    fix code

--- src/pages/cashier.vue
+++
@@ -1,6 +0,0 @@
-<script setup>
-</script>
-
-<template>
-<div> this is my cashier</div>
-</template>
--- src/pages/manager.vue
+++
@@ -1,6 +0,0 @@
-<script setup>
-</script>
-
-<template>
-<div> this is my mananger page</div>
-</template>
