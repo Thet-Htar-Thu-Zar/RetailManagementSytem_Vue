771c6e7d675f2ea7de175c628046053c60dd6c8b
Thet-Htar-Thu-Zar <thethtarthuzar648@gmail.com>
2025-01-17 08:31:58 +0630

    fix code

--- src/modules/stock/StockView.vue
+++ src/modules/stock/StockView.vue
@@ -49,56 +49,6 @@ import { computed } from 'vue';
   const totalItemsInCart = computed(() => cartStore.cartItems.reduce((total, item) => total + item.quantity, 0) );
   </script>
 
-  <!-- <template>
-
-      <section class="p-3 h-full">
-      <TableCaption class="w-full text-5xl flex justify-center">Stock Page</TableCaption>
-      <Button class="flex hover:bg-green-600 text-white px-4 py-2 rounded pr-7 pl-7 ml-auto"> 
-        <RouterLink to="/cart" class="flex">
-          <LucideShoppingCart class="h-4 w-4 text-8xl" />
-          {{ cartStore.totalItems }}</RouterLink> 
-        </Button>
-
-      <br>
-      <Table>
-      <TableHeader>
-        <TableRow>
-          
-          <TableHead>Product Name</TableHead>
-          <TableHead>Remaining Stock</TableHead>
-          <TableHead>Product Price</TableHead>
-          <TableHead >Product Profit</TableHead>
-          <TableHead class="ml-30">Action</TableHead>
-
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        <TableRow v-for="products in data" :key="products.productID">
-          
-          <TableCell class="w-[200px]">{{ products.productName }}</TableCell>
-          <TableCell class="w-[200px]">{{ products.remainingStock }}</TableCell>
-          <TableCell class="w-[200px]">{{ products.productPrice }}</TableCell>
-          <TableCell class="w-[200px]">{{ products.productProfit }}</TableCell>
-          <TableCell ><Button class="bg-sky-500/100 mr-4"
-            @click = "addToCart({
-              id: products.productID,
-              name: products.productName,
-              price: products.productPrice,
-            })">
-            Add to Cart <LucideShoppingCart/></Button>
-            <Button class="bg-red-500"
-            @click = "removeFromCart(products.productID)">
-              Remove From Cart <LucideShoppingCart/></Button>
-            
-            </TableCell>
-          
-        </TableRow>
-      </TableBody>
-      </Table>
-
-      </section>
-  </template> -->
-
   <template>
     <section class="p-3 h-full">
       <TableCaption class="w-full text-5xl flex justify-center">Stock Page</TableCaption>
