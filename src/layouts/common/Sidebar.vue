41b4d824ea235cf3794662345c65f9e9179d3451
Thet-Htar-Thu-Zar <thethtarthuzar648@gmail.com>
2025-01-17 14:02:40 +0630

    change sidebar view

--- src/layouts/common/Sidebar.vue
+++ src/layouts/common/Sidebar.vue
@@ -6,7 +6,7 @@ import { Bell, Home, LineChart, Package, Package2, ShoppingCart, Users } from 'l
 
 <template>
   <div class="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-    <div class="hidden border-r bg-muted/40 md:block">
+    <div class="hidden border-r bg-gradient-to-r from-green-200 to-blue-300 md:block">
       <div class="flex h-full max-h-screen flex-col gap-2">
         <div class="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
           <a href="/" class="flex items-center gap-2 font-semibold">
@@ -20,16 +20,16 @@ import { Bell, Home, LineChart, Package, Package2, ShoppingCart, Users } from 'l
         </div>
         <div class="flex-1">
           <nav class="grid items-start px-2 text-sm font-medium lg:px-4">
-            <RouterLink
+            <!-- <RouterLink
               to="/"
               class="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary pb-4"
             >
               <Home class="h-4 w-4" />
               Dashboard
-            </RouterLink>
+            </RouterLink> -->
         
             <RouterLink
-              to="/stock"
+              to="/"
               class="flex items-center gap-3 rounded-lg  px-3 py-2 text-muted-foreground transition-all hover:text-primary pb-4"
             >
               <Package class="h-4 w-4" />
