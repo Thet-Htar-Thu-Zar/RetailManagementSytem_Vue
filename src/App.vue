4284342daa7915dd59829c0350274229b7b1b1f0
Thet-Htar-Thu-Zar <thethtarthuzar648@gmail.com>
2025-01-21 15:41:15 +0630

    add toast

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,10 @@
 <script setup lang="ts">
+import { Toaster } from './components/ui/toast';
+
 
 </script>
 
 <template>
-  <!-- <div class="flex">
-  <div>
-    <aside class="w-64 bg-gray-600 text-white h-full">
-      <div class="p-4 text-center font-bold text-lg">
-        Dashboard
-      </div>
-      <nav>
-        <ul>
-          <li>
-            
-            <router-link to="/stock" class="block py-2 px-4 hover:bg-pink-500">Stock</router-link>
-          </li>
-          <li>
-            <router-link to="/cart" class="block py-2 px-4 hover:bg-pink-500">Cart</router-link>
-          </li>
-          <li>
-            <router-link to="/manager" class="block py-2 px-4 hover:bg-pink-500">Manager</router-link>
-          </li>
-        </ul>
-      </nav>
-    </aside>  
-  </div>
-  <div>
-    <RouterView/>
-  </div>
-</div> -->
-<!-- <StockView/> -->
- <RouterView/>
+  <Toaster />
+  <RouterView />
 </template>
-
