a602ec8675aee836ca5d437650c15ef9641234b4
Thet-Htar-Thu-Zar <thethtarthuzar648@gmail.com>
2025-01-23 16:40:12 +0630

    add dashboard

--- src/layouts/common/Sidebar.vue
+++ src/layouts/common/Sidebar.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
-
 import { Button } from '@/components/ui/button'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
+import { useColorMode } from '@vueuse/core';
+import { ref } from 'vue';
 import { Bell, Home, LineChart, Package, Package2, ShoppingCart, Users } from 'lucide-vue-next'
+import { Icon } from '@iconify/vue'
+const mode = useColorMode()
+
 </script>
 
 <template>
@@ -9,7 +14,7 @@ import { Bell, Home, LineChart, Package, Package2, ShoppingCart, Users } from 'l
     <div class="hidden border-r bg-gradient-to-r from-green-200 to-blue-300 md:block">
       <div class="flex h-full max-h-screen flex-col gap-2">
         <div class="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
-          <a href="/" class="flex items-center gap-2 font-semibold">
+          <a href="/" class="flex items-center gap-2 mr-5 font-semibold">
             <Package2 class="h-6 w-6" />
             <span class="">Chris Market</span>
           </a>
@@ -17,46 +22,60 @@ import { Bell, Home, LineChart, Package, Package2, ShoppingCart, Users } from 'l
             <Bell class="h-4 w-4" />
             <span class="sr-only">Toggle notifications</span>
           </Button> -->
+
+          <DropdownMenu>
+            <DropdownMenuTrigger as-child>
+              <Button variant="outline">
+                <Icon icon="radix-icons:moon"
+                  class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                <Icon icon="radix-icons:sun"
+                  class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                <span class="sr-only">Toggle theme</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem @click="mode = 'light'">
+                Light
+              </DropdownMenuItem>
+              <DropdownMenuItem @click="mode = 'dark'">
+                Dark
+              </DropdownMenuItem>
+              <DropdownMenuItem @click="mode = 'auto'">
+                System
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+
         </div>
         <div class="flex-1">
           <nav class="grid items-start px-2 text-sm font-medium lg:px-4">
-            <!-- <RouterLink
-              to="/"
-              class="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary pb-4"
-            >
+            <RouterLink to="/"
+              class="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary pb-4">
               <Home class="h-4 w-4" />
               Dashboard
-            </RouterLink> -->
-        
-            <RouterLink
-              to="/"
-              class="flex items-center gap-3 rounded-lg  px-3 py-2 text-muted-foreground transition-all hover:text-primary pb-4"
-            >
+            </RouterLink>
+
+            <RouterLink to="/product"
+              class="flex items-center gap-3 rounded-lg  px-3 py-2 text-muted-foreground transition-all hover:text-primary pb-4">
               <Package class="h-4 w-4" />
               Product Stock
             </RouterLink>
 
-            <RouterLink
-              to="/cart"
-              class="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary pb-4"
-            >
+            <RouterLink to="/cart"
+              class="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary pb-4">
               <ShoppingCart class="h-4 w-4" />
               Cart
-         
+
             </RouterLink>
-          
-            <RouterLink
-              to="/cashier"
-              class="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary pb-4"
-            >
+
+            <RouterLink to="/cashier"
+              class="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary pb-4">
               <LineChart class="h-4 w-4" />
               Cashier
             </RouterLink>
-             
-            <RouterLink
-              to="/manager"
-              class="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary pb-4"
-            >
+
+            <RouterLink to="/manager"
+              class="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary pb-4">
               <Users class="h-4 w-4" />
               Manager View
             </RouterLink>
@@ -64,6 +83,6 @@ import { Bell, Home, LineChart, Package, Package2, ShoppingCart, Users } from 'l
         </div>
       </div>
     </div>
-   
+
   </div>
 </template>
